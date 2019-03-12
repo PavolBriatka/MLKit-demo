67985467b41b7abc2640a05dd2fb02b9cddfeaf3
Pavol Briatka <pavol.briatka@redcloudtechnology.com>
2019-03-12 11:02:57 +0000

    Create scanner activity and update manifest file

---
+++ app/src/main/java/com/example/redmlkitdemo/scanner/ScannerActivity.kt
@@ -0,0 +1,13 @@
+package com.example.redmlkitdemo.scanner
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.redmlkitdemo.R
+
+class ScannerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scanner)
+    }
+}
