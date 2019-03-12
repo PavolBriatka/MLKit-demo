0b6f32aea5790b69628b7375f8b69c9ea5de24bb
Pavol Briatka <pavol.briatka@redcloudtechnology.com>
2019-03-12 09:35:33 +0000

    First commit

---
+++ app/src/main/java/com/example/redmlkitdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.redmlkitdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
