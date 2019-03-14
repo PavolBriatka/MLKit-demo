1aa0eed6364356898536606760e3939d7eeb169c
Pavol Briatka <pavol.briatka@redcloudtechnology.com>
2019-03-14 16:44:24 +0000

    Move permission logic to main activity

--- app/src/main/java/com/example/redmlkitdemo/MainActivity.kt
+++ app/src/main/java/com/example/redmlkitdemo/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.example.redmlkitdemo
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import com.example.redmlkitdemo.scanner.ScannerActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val PERMISSION_REQUESTS = 10
+
+        private fun isPermissionGranted(context: Context, permission: String): Boolean {
+            if (ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED) {
+                return true
+            }
+            return false
+        }
+    }
+
+    private val requiredPermissions: Array<String?>
+        get() {
+            return try {
+                val info = this.packageManager
+                    .getPackageInfo(this.packageName, PackageManager.GET_PERMISSIONS)
+                val ps = info.requestedPermissions
+                if (ps != null && ps.isNotEmpty()) {
+                    ps
+                } else {
+                    arrayOfNulls(0)
+                }
+            } catch (e: Exception) {
+                arrayOfNulls(0)
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +46,34 @@ class MainActivity : AppCompatActivity() {
         scanner_button.setOnClickListener {
             val scannerIntent = Intent(this, ScannerActivity::class.java)
             startActivity(scannerIntent)
+        }
+
+        if (!allPermissionsGranted()) {
+            getRuntimePermissions()
+        }
+    }
+
+    private fun allPermissionsGranted(): Boolean {
+        for (permission in requiredPermissions) {
+            if (!MainActivity.isPermissionGranted(this, permission!!)) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun getRuntimePermissions() {
+        val allNeededPermissions = arrayListOf<String>()
+        for (permission in requiredPermissions) {
+            if (!MainActivity.isPermissionGranted(this, permission!!)) {
+                allNeededPermissions.add(permission)
+            }
+        }
+
+        if (!allNeededPermissions.isEmpty()) {
+            ActivityCompat.requestPermissions(
+                this, allNeededPermissions.toTypedArray(), MainActivity.PERMISSION_REQUESTS
+            )
         }
     }
 }
